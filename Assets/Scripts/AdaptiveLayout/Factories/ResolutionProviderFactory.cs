357fad60e991e373420c7ce0e0f211e5ac6adde6
Anastasiya <anastasiya.kolushenko@gmail.com>
2025-06-20 18:07:50 +0400

    AL-removed-static-from-factory-add-warn-about-di-container

---
+++ Assets/Scripts/AdaptiveLayout/Factories/ResolutionProviderFactory.cs
@@ -0,0 +1,25 @@
+namespace AdaptiveLayout
+{ 
+    // NOTE: If using a DI container (e.g., Zenject or VContainer),
+    // this factory should be removed and replaced with platform-specific 
+    // dependency binding inside the Installer.
+    
+    public interface IResolutionProviderFactory
+    {
+        public IResolutionProvider GetResolutionProvider();
+    }
+    
+    public class ResolutionProviderFactory : IResolutionProviderFactory
+    {
+        public IResolutionProvider GetResolutionProvider()
+        {
+#if UNITY_IOS
+            return new IOSResolutionProvider();
+#elif UNITY_ANDROID
+            return new AndroidResolutionProvider();
+#else
+            return new DefaultResolutionProvider();
+#endif
+        }
+    }
+}
